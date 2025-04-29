a19954fb8ebe8e3cd55e81974f567269c847e205
unknown <kadufilho196@gmail.com>
2025-04-29 17:33:49 -0300

    atualizando projeto

--- src/main/java/org/example/entities/FormaPagamento.java
+++ src/main/java/org/example/entities/FormaPagamento.java
@@ -14,12 +14,18 @@ public class FormaPagamento  implements Serializable {
     @Column(name = "FPG_DESCRICAO")
     private static String fpgDescricao;
 
+    @Column(name = "FPG_NOME")
+    private static String fpgNome;
+    @Column(name = "FPG_PRICE")
+    private static String fpgPrice;
     public FormaPagamento() {
     }
 
-    public FormaPagamento(Long fpgId, String fpgDescricao) {
+    public FormaPagamento(Long fpgId, String fpgDescricao, String fpgNome, String fpgPrice) {
         this.fpgId = fpgId;
         this.fpgDescricao = fpgDescricao;
+        this.fpgNome = fpgNome;
+        this.fpgPrice = fpgPrice;
     }
 
     public Long getFpgId() {
@@ -38,4 +44,19 @@ public class FormaPagamento  implements Serializable {
         this.fpgDescricao = fpgDescricao;
     }
 
+    public static String getFpgNome() {
+        return fpgNome;
+    }
+
+    public static void setFpgNome(String fpgNome) {
+        FormaPagamento.fpgNome = fpgNome;
+    }
+
+    public static String getFpgPrice() {
+        return fpgPrice;
+    }
+
+    public static void setFpgPrice(String fpgPrice) {
+        FormaPagamento.fpgPrice = fpgPrice;
+    }
 }
