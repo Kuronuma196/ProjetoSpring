08be405576282f5dc86f93cf8e8d4fa11cc21bd7
Kuronuma196 <kadufilho196@gmail.com>
2025-05-31 12:02:56 -0300

    atualizacaoTestes

---
+++ src/main/test/java/ProductDeleteTest.java
@@ -0,0 +1,21 @@
+public class ProductDeleteTest extends BaseTest {
+
+    @Test
+    public void testDeleteProductSuccessfully() {
+        driver.findElement(By.cssSelector(".btn-delete")).click();
+        driver.findElement(By.id("btn-confirmar-exclusao")).click();
+
+        // Verifica se o produto sumiu
+        boolean produtoExcluido = driver.findElements(By.xpath("//*[contains(text(), 'Produto Teste')]")).isEmpty();
+        assert(produtoExcluido);
+    }
+
+    @Test
+    public void testDeleteWithoutSelectingAnything() {
+        // Dependendo da interface, pode haver verificação se não há item selecionado
+        driver.findElement(By.cssSelector(".btn-delete")).click();
+
+        WebElement errorMsg = driver.findElement(By.className("error-msg"));
+        assert(errorMsg.isDisplayed());
+    }
+}
