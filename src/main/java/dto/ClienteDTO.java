2119ec22b87b94b5b6b0b2aaa8697589b3a1da29
Kuronuma196 <kadufilho196@gmail.com>
2025-06-10 17:18:30 -0300

    Atualizando Menu

---
+++ src/main/java/dto/ClienteDTO.java
@@ -0,0 +1,142 @@
+package dto;
+
+public class ClienteDTO {
+    private Long cliId;
+    private String cliNome;
+    private String cliCpf;
+    private String cliEmail;
+    private String cliTelefone;
+
+    private String endRua;
+    private String endNumero;
+    private String endCidade;
+    private String endCep;
+    private String endEstado;
+
+    private String conCelular;
+    private String conTelefoneComercial;
+    private String conEmail;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Long cliId, String cliNome, String cliCpf, String cliEmail, String cliTelefone, String endRua, String endNumero, String endCidade, String endCep, String endEstado, String conCelular, String conTelefoneComercial, String conEmail) {
+        this.cliId = cliId;
+        this.cliNome = cliNome;
+        this.cliCpf = cliCpf;
+        this.cliEmail = cliEmail;
+        this.cliTelefone = cliTelefone;
+        this.endRua = endRua;
+        this.endNumero = endNumero;
+        this.endCidade = endCidade;
+        this.endCep = endCep;
+        this.endEstado = endEstado;
+        this.conCelular = conCelular;
+        this.conTelefoneComercial = conTelefoneComercial;
+        this.conEmail = conEmail;
+    }
+
+    public Long getCliId() {
+        return cliId;
+    }
+
+    public void setCliId(Long cliId) {
+        this.cliId = cliId;
+    }
+
+    public String getCliNome() {
+        return cliNome;
+    }
+
+    public void setCliNome(String cliNome) {
+        this.cliNome = cliNome;
+    }
+
+    public String getCliCpf() {
+        return cliCpf;
+    }
+
+    public void setCliCpf(String cliCpf) {
+        this.cliCpf = cliCpf;
+    }
+
+    public String getCliEmail() {
+        return cliEmail;
+    }
+
+    public void setCliEmail(String cliEmail) {
+        this.cliEmail = cliEmail;
+    }
+
+    public String getCliTelefone() {
+        return cliTelefone;
+    }
+
+    public void setCliTelefone(String cliTelefone) {
+        this.cliTelefone = cliTelefone;
+    }
+
+    public String getEndRua() {
+        return endRua;
+    }
+
+    public void setEndRua(String endRua) {
+        this.endRua = endRua;
+    }
+
+    public String getEndNumero() {
+        return endNumero;
+    }
+
+    public void setEndNumero(String endNumero) {
+        this.endNumero = endNumero;
+    }
+
+    public String getEndCidade() {
+        return endCidade;
+    }
+
+    public void setEndCidade(String endCidade) {
+        this.endCidade = endCidade;
+    }
+
+    public String getEndCep() {
+        return endCep;
+    }
+
+    public void setEndCep(String endCep) {
+        this.endCep = endCep;
+    }
+
+    public String getEndEstado() {
+        return endEstado;
+    }
+
+    public void setEndEstado(String endEstado) {
+        this.endEstado = endEstado;
+    }
+
+    public String getConCelular() {
+        return conCelular;
+    }
+
+    public void setConCelular(String conCelular) {
+        this.conCelular = conCelular;
+    }
+
+    public String getConTelefoneComercial() {
+        return conTelefoneComercial;
+    }
+
+    public void setConTelefoneComercial(String conTelefoneComercial) {
+        this.conTelefoneComercial = conTelefoneComercial;
+    }
+
+    public String getConEmail() {
+        return conEmail;
+    }
+
+    public void setConEmail(String conEmail) {
+        this.conEmail = conEmail;
+    }
+}
