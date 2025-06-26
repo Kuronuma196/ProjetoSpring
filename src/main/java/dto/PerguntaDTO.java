cb9faca6f17490609b095f310d2909d7a9c1c783
Kuronuma196 <kadufilho196@gmail.com>
2025-06-26 17:29:30 -0300

    Start Pesquisa

---
+++ src/main/java/dto/PerguntaDTO.java
@@ -0,0 +1,14 @@
+package dto;
+
+public class PerguntaDTO {
+    private String pergunta;
+    // getters e setters
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+}
