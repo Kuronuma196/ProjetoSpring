ef85015f93316d313b72290e5149a7144018838c
Kuronuma196 <kadufilho196@gmail.com>
2025-07-29 17:29:59 -0300

    Repage

--- src/main/test/java/TestFor/BaseTest2.java
+++ src/main/test/java/TestFor/BaseTest2.java
@@ -1,4 +1,18 @@
-package org.example.test.javaTes.TestFor;
+package TestFor;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class BaseTest2 {
+    protected WebDriver driver;
+    @Test
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://localhost:4200");
+    }
+    public void tearDown() {
+        driver.quit();
+    }
 }
--- src/main/test/java/TestFor/FornecedorCreateTest.java
+++ src/main/test/java/TestFor/FornecedorCreateTest.java
@@ -1,4 +1,40 @@
 package TestFor;
 
-public class FornecedorCreateTest {
+import TestPro.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+public class FornecedorCreateTest extends BaseTest {
+
+    @Test
+    public void testCreateFornecedorSuccessfully() {
+        WebElement btnNovoFornecedor = driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-crud/button"));
+        btnNovoFornecedor.click();
+
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[1]/div[1]/div[2]/div/input")).sendKeys("Fornecedor Teste");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[2]/div[1]/div[2]/div/input")).sendKeys("5.50");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[3]/div[1]/div[2]/div/input")).sendKeys("8.90");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[4]/div[1]/div[2]/div/input")).sendKeys("100");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[5]/div[1]/div[2]/div/input")).sendKeys("Testes");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[6]/div[1]/div[2]/div/input")).sendKeys("9876543210123");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[7]/div[1]/div[2]/div/input")).sendKeys("Marca Teste");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[8]/div[1]/div[2]/div/input")).sendKeys("Unidade");
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/form/mat-form-field[9]/div[1]/div[2]/div/input")).sendKeys("ativo"); // Se for checkbox
+
+
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/button[1]")).click(); // botão Salvar
+
+        // Verificação básica
+        WebElement created = driver.findElement(By.xpath("//*[contains(text(), 'Produto Teste')]"));
+        assert(created.isDisplayed());
+    }
+
+    @Test
+    public void testCreateProductWithEmptyFields() {
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-crud/button")).click();
+        driver.findElement(By.xpath("/html/body/app-root/app-nav/mat-sidenav-container/mat-sidenav-content/app-product-create/mat-card/button[1]")).click();
+
+        WebElement errorMsg = driver.findElement(By.className("error-msg"));
+        assert(errorMsg.isDisplayed());
+    }
 }
